05356fb3f92e1d8cfc5c832b26bc91a0434f011c
STEVEN2199 <stevenguillermo_encalada@hotmail.com>
2022-11-26 20:06:58 -0500

    COMENTARIOS SOBRE PROYECTO

--- CountingThings.py
+++ CountingThings.py
@@ -22,19 +22,23 @@ cnts,_ = cv2.findContours(th, cv2.RETR_EXTERNAL,
 cv2.drawContours(imagen, cnts, -1, (255,0,0),2)
 print('Contornos: ', len(cnts))
 
+#ME PERMITE PONER TEXTO SOBRE UNA IMAGEN
 font = cv2.FONT_HERSHEY_SIMPLEX
 i=0
 for c in cnts:
+    #Podemos encontrar el centro de la mancha usando momentos en OpenCV.
 	M=cv2.moments(c)
 	if (M["m00"]==0): M["m00"]=1
 	x=int(M["m10"]/M["m00"])
 	y=int(M['m01']/M['m00'])
 
-	mensaje = 'Objeto #:' + str(i+1)
-	cv2.putText(imagen,mensaje,(x-40,y),font,0.75,
+	mensaje = 'Objeto#:' + str(i+1)
+	#ME PERMITE PONER TEXTO SOBRE UNA IMAGEN
+	cv2.putText(clon,mensaje,(x-40,y),font,0.75,
 		(255,0,0),2,cv2.LINE_AA)
-	cv2.drawContours(imagen, [c], 0, (255,0,0),2)
-	cv2.imshow('Imagen', imagen)
+	#DIBUJA LOS CONTRONOS SOBRE MI IMAGEN
+	cv2.drawContours(clon, [c], 0, (255,0,0),2)
+	cv2.imshow('Imagen', clon)
 	cv2.waitKey(0)
 	i = i+1
 cv2.destroyAllWindows()
